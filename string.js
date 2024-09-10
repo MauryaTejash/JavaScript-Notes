45a3326c4caeac186349bdcf739245d4109eacda
Tejash Maurya <mauryatejash00@gmail.com>
2024-09-10 23:14:03 +0530

    All About the string

---
+++ string.js
@@ -0,0 +1,30 @@
+let val = "Tejash"
+let val1 = 10
+
+// console.log(val +" "+ val1 + "Maurya");
+
+
+console.log(`My Name is ${val} and count value is ${val1}`);
+const name1 = String("Maurya")
+const name2 = new String("hello%20Tejash")
+
+console.table([name2,name1]);
+
+
+console.log(name2[4]);
+// console.log(name.__proto__);
+
+console.log(name2.toUpperCase());
+console.log(name2.trim());
+
+
+const naam = name2.substring(0,4)
+console.log(naam);
+
+const nam = name2.slice(-2,4);
+console.log(nam);
+
+console.log(name2.replace('%20','-'));
+console.log(name2.includes('heel'));
+
+
