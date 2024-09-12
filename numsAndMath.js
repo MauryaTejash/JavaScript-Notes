5deba225062a461238f902b28b72a5a53e291910
Tejash Maurya <mauryatejash00@gmail.com>
2024-09-12 22:57:01 +0530

    Math function details

---
+++ numsAndMath.js
@@ -0,0 +1,54 @@
+// let val = 300
+// console.log(typeof val);
+
+// let val =new Number(399.8796)
+
+// console.log(val.toString());
+// console.log(val.toString().length);
+// console.log(val.toFixed(2));
+
+// console.log(val.toPrecision(4));
+
+
+// console.log(typeof val);
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.abs(-4));
+// console.log(Math.floor(4.6));
+// console.log(Math.pow(2,3));
+// console.log(Math.sqrt(26));
+// console.log(Math.max(10,21,14,23));
+
+// //generate the random values 
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random()*10 + 1));
+
+
+// //generate the random values  in given range
+// const min = 10
+// const max = 20
+// console.log(Math.random());
+
+// console.log(Math.floor(Math.random()* (max - min + 1) + min));
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let date = new Date()
+console.log(date);
+
+console.log(date.toString());
+console.log(date.toISOString());
+
+
+
+
+
+
+
